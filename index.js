0e717115f3b61b75574584182fe9140cc1b8c3a6
Bin <binlin35@yahoo.com>
2021-04-08 14:03:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4';
+dodger.style.bottom = '0px';
+dodger.style.left = '0px';
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  } 
+  const lefNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(lefNumbers, 10)
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  }); 
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 
+function moveDodgerRight() {
+  const lefNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(lefNumbers, 10)
+  if(left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
 
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  } 
+function moveDodgerDown() {
+  const bottomNum = dodger.style.bottom.replace('px', '')
+  const bottom = parseInt(bottomNum, 10)
+  if(bottom > 0) {
+    dodger.style.bottom = `${bottom - 1}px`
+  }
+}
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  }); 
+function moveDodgerUp() {
+  const bottomNum = dodger.style.bottom.replace('px', '')
+  const bottom = parseInt(bottomNum, 10)
+  if(bottom < 380) {
+    dodger.style.bottom = `${bottom + 1}px`
+  }
+}
+
+document.addEventListener('keydown',(e) => {
+  if(e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  }else if(e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }else if(e.key === 'ArrowDown') {
+    moveDodgerDown();
+  }else if(e.key === 'ArrowUp') {
+    moveDodgerUp();
+  }
+});
